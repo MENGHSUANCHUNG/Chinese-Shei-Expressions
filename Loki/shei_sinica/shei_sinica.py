45464ecb45b4961a57373a73cd810a21ee2e6a4d
MENGHSUANCHUNG <123518774+MENGHSUANCHUNG@users.noreply.github.com>
2025-04-11 21:41:00 +0800

    upload appendices

--- Loki/shei_sinica/shei_sinica.py
+++ Loki/shei_sinica/shei_sinica.py
@@ -301,7 +301,7 @@ def testIntent():
 if __name__ == "__main__":
     
                   
-    inputLIST = ["知道誰和誰關係如何"]
+    inputLIST = ["誰勝誰負最後才知道"]
 
     for i, inputSTR in enumerate(inputLIST, start=1):  
         resultDICT = execLoki([inputSTR])
